67101174adab0b11f50d6aff0b5fd2a96fcf0302
Paavveel <pa.tomilin@gmail.com>
2022-11-16 18:18:30 +0300

    feat: implement Login into DashHeader component and login logic

--- src/components/DashHeader/DashHeader.tsx
+++ src/components/DashHeader/DashHeader.tsx
@@ -1,13 +1,45 @@
-import { Link } from 'react-router-dom';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import cn from 'classnames';
+import { useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSendLogoutMutation } from '../../features/auth/authApiSlice';
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 export const DashHeader = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [sendLogout, { isLoading, isSuccess, isError }] =
+    useSendLogoutMutation();
+
+  useEffect(() => {
+    if (isSuccess) navigate('/');
+  }, [isSuccess, navigate]);
+
+  if (isLoading) return <p>Logging Out...</p>;
+  if (isError) return <p>Не удалось выйти</p>;
   return (
     <header className='dash-header'>
-      <div className='dash-header__container'>
+      <div
+        className={cn('dash-header__container', {
+          'dash-header__container--small':
+            !DASH_REGEX.test(pathname) &&
+            !NOTES_REGEX.test(pathname) &&
+            !USERS_REGEX.test(pathname),
+        })}
+      >
         <Link to='/dash'>
           <h1 className='dash-header__title'>Tech Notes</h1>
         </Link>
-        <nav className='dash-header__nav'>{/* add nav buttons later */}</nav>
+        <nav className='dash-header__nav'>
+          <button className='icon-button' title='Logout' onClick={sendLogout}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </button>
+        </nav>
       </div>
     </header>
   );
--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,3 +1,114 @@
+import cn from 'classnames';
+import { RefObject, useRef, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { setCredentials } from '.';
+import { isFetchBaseQueryError } from '../../app/api/helpers';
+import { useAppDispatch } from '../../app/hooks';
+import { useLoginMutation } from './authApiSlice';
+import { ICredential } from './authTypes';
+
 export const Login = () => {
-  return <div>Login</div>;
+  const errRef = useRef() as RefObject<HTMLParagraphElement> | null;
+  const [errMsg, setErrMsg] = useState('');
+  const [login, { isLoading, isError }] = useLoginMutation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ICredential>();
+
+  const onLogin: SubmitHandler<ICredential> = async (data) => {
+    try {
+      const { accessToken } = await login(data).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      navigate('/dash');
+    } catch (err) {
+      if (err) {
+        if (isFetchBaseQueryError(err)) {
+          // you can access all properties of `FetchBaseQueryError` here
+          const errMsg = 'error' in err ? err.error : JSON.stringify(err.data);
+          if (!err.status) {
+            setErrMsg('No Server Response');
+          } else if (err.status === 400) {
+            setErrMsg('Missing Username or Password');
+          } else if (err.status === 401) {
+            setErrMsg('Unauthorized');
+          } else {
+            setErrMsg(errMsg);
+          }
+        }
+      }
+
+      if (errRef?.current) {
+        errRef.current.focus();
+      }
+    }
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <section className='public'>
+      <header>
+        <h1>Вход для сотрудников</h1>
+      </header>
+      <main className='login'>
+        {isError && (
+          <p ref={errRef} className='errmsg' aria-live='assertive'>
+            {errMsg}
+          </p>
+        )}
+
+        <form className='form' onSubmit={handleSubmit(onLogin)}>
+          <label className='form__label' htmlFor='username'>
+            Имя пользователя:
+          </label>
+          <input
+            {...register('username', {
+              required: {
+                value: true,
+                message: 'Это обязательное поле',
+              },
+            })}
+            className={cn('form__input', {
+              'form__input--incomplete': errors.username,
+            })}
+            id='username'
+            type='text'
+            autoComplete='off'
+          />
+          {errors.username?.message && (
+            <p className='errmsg'>{errors.username.message}</p>
+          )}
+          <label className='form__label' htmlFor='password'>
+            Пароль:
+          </label>
+          <input
+            {...register('password', {
+              required: {
+                value: true,
+                message: 'Это обязательное поле',
+              },
+            })}
+            className={cn('form__input', {
+              'form__input--incomplete': errors.password,
+            })}
+            id='password'
+            type='password'
+          />
+          {errors.password?.message && (
+            <p className='errmsg'>{errors.password.message}</p>
+          )}
+          <button className='form__submit-button'>Войти</button>
+        </form>
+      </main>
+      <footer>
+        <Link to='/'>На главную</Link>
+      </footer>
+    </section>
+  );
 };
