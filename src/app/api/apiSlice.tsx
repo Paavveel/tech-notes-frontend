938a48ee2b91b7ca9b4beffe18adbf7d2ed7aa07
Paavveel <pa.tomilin@gmail.com>
2022-11-17 16:43:15 +0300

    feat: add baseQueryWithReauth to implement reauth logic

--- src/app/api/apiSlice.tsx
+++ src/app/api/apiSlice.tsx
@@ -1,7 +1,69 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+import { setCredentials } from '../../features/auth';
+import { IAuthResponse } from '../../features/auth/authTypes';
+import { RootState } from '../store';
+
+const BASE_URL = 'http://localhost:3500';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: 'include',
+  prepareHeaders(headers, { getState }) {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set(
+        'authorization',
+        `
+          Bearer ${token}
+        `
+      );
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // console.log(args) // request url, method, body
+  // console.log(api) // signal, dispatch, getState()
+  // console.log(extraOptions) //custom like {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  // If you want, handle other status codes, too
+  if (result?.error?.status === 403) {
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials(refreshResult.data as IAuthResponse));
+
+      // retry original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        (refreshResult.error.data as any).message = 'You are not logged in. ';
+      }
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Note', 'User'],
   endpoints: (builder) => ({}),
 });
