e0c027c81d13041204610d2c7d051e3f604b8f98
Paavveel <pa.tomilin@gmail.com>
2022-11-18 15:23:46 +0300

    feat: add useAut in the Welcome page and DashHeader

--- src/components/DashHeader/DashHeader.tsx
+++ src/components/DashHeader/DashHeader.tsx
@@ -1,27 +1,109 @@
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFileCirclePlus,
+  faFilePen,
+  faRightFromBracket,
+  faUserGear,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import cn from 'classnames';
 import { useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useSendLogoutMutation } from '../../features/auth/authApiSlice';
+import { useAuth } from '../../hooks/useAuth';
 
 const DASH_REGEX = /^\/dash(\/)?$/;
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
 const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 export const DashHeader = () => {
+  const { isManager, isAdmin } = useAuth();
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
   const [sendLogout, { isLoading, isSuccess, isError }] =
     useSendLogoutMutation();
 
+  const onNewNoteClicked = () => navigate('/dash/notes/new');
+  const onNewUserClicked = () => navigate('/dash/users/new');
+  const onNotesClicked = () => navigate('/dash/notes');
+  const onUsersClicked = () => navigate('/dash/users');
+
+  let newNoteButton = null;
+  if (NOTES_REGEX.test(pathname)) {
+    newNoteButton = (
+      <button
+        className='icon-button'
+        title='New Note'
+        onClick={onNewNoteClicked}
+      >
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </button>
+    );
+  }
+
+  let newUserButton = null;
+  if (isManager || isAdmin) {
+    if (USERS_REGEX.test(pathname)) {
+      newUserButton = (
+        <button
+          className='icon-button'
+          title='New User'
+          onClick={onNewUserClicked}
+        >
+          <FontAwesomeIcon icon={faUserPlus} />
+        </button>
+      );
+    }
+  }
+  let userButton = null;
+  if (isManager || isAdmin) {
+    if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+      userButton = (
+        <button className='icon-button' title='Users' onClick={onUsersClicked}>
+          <FontAwesomeIcon icon={faUserGear} />
+        </button>
+      );
+    }
+  }
+
+  let notesButton = null;
+  if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
+    notesButton = (
+      <button className='icon-button' title='Notes' onClick={onNotesClicked}>
+        <FontAwesomeIcon icon={faFilePen} />
+      </button>
+    );
+  }
+
+  const logoutButton = (
+    <button className='icon-button' title='Logout' onClick={sendLogout}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  );
+
   useEffect(() => {
     if (isSuccess) navigate('/');
   }, [isSuccess, navigate]);
 
-  if (isLoading) return <p>Logging Out...</p>;
-  if (isError) return <p>Не удалось выйти</p>;
+  let buttonContent;
+  if (isLoading) {
+    buttonContent = <p>Logging Out...</p>;
+  } else if (isError) {
+    <p>Не удалось выйти</p>;
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {userButton}
+        {logoutButton}
+      </>
+    );
+  }
   return (
     <header className='dash-header'>
       <div
@@ -35,11 +117,7 @@ export const DashHeader = () => {
         <Link to='/dash'>
           <h1 className='dash-header__title'>Tech Notes</h1>
         </Link>
-        <nav className='dash-header__nav'>
-          <button className='icon-button' title='Logout' onClick={sendLogout}>
-            <FontAwesomeIcon icon={faRightFromBracket} />
-          </button>
-        </nav>
+        <nav className='dash-header__nav'>{buttonContent}</nav>
       </div>
     </header>
   );
--- src/features/auth/Welcome.tsx
+++ src/features/auth/Welcome.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 export const Welcome = () => {
+  const { username, isManager, isAdmin } = useAuth();
   const date = new Date();
   const today = new Intl.DateTimeFormat('ru', {
     dateStyle: 'full',
@@ -11,7 +13,7 @@ export const Welcome = () => {
     <section className='welcome'>
       <p>{today}</p>
 
-      <h1>Добро пожаловать!</h1>
+      <h1>Добро пожаловать {username}!</h1>
 
       <p>
         <Link to='/dash/notes'>Смотреть заметки</Link>
@@ -20,12 +22,16 @@ export const Welcome = () => {
         <Link to='/dash/notes/new'>Добавить новую заметку</Link>
       </p>
 
-      <p>
-        <Link to='/dash/users'>Настройки пользователя</Link>
-      </p>
-      <p>
-        <Link to='/dash/users/new'>Добавить нового пользователя</Link>
-      </p>
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to='/dash/users'>Настройки пользователя</Link>
+        </p>
+      )}
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to='/dash/users/new'>Добавить нового пользователя</Link>
+        </p>
+      )}
     </section>
   );
 };
