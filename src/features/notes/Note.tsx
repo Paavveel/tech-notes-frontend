792e636e619dcf3f0a7b69e463d47aa8d086dc02
Paavveel <pa.tomilin@gmail.com>
2022-11-21 14:00:00 +0300

    fix: use cache for notes

--- src/features/notes/Note.tsx
+++ src/features/notes/Note.tsx
@@ -2,63 +2,50 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import PulseLoader from 'react-spinners/PulseLoader';
-import { useGetNoteByIdQuery } from './notesApiSlice';
 import { INote } from './notesTypes';
 
-export const Note = memo(({ id }: INote) => {
-  const navigate = useNavigate();
-  const {
-    data: note,
-    isLoading,
-    isError,
-  } = useGetNoteByIdQuery(id, {
-    refetchOnReconnect: true,
-  });
+export const Note = memo(
+  ({ completed, createdAt, updatedAt, username, title, id }: INote) => {
+    const navigate = useNavigate();
 
-  if (isLoading) return <PulseLoader color='#FFF' />;
+    const created = new Date(createdAt).toLocaleString('ru', {
+      day: 'numeric',
+      month: 'numeric',
+      year: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
 
-  if (!note) {
-    return <p>error</p>;
+    const updated = new Date(updatedAt).toLocaleString('ru', {
+      day: 'numeric',
+      month: 'numeric',
+      year: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+
+    const handleEdit = () => navigate(`/dash/notes/${id}`);
+
+    return (
+      <tr className='table__row'>
+        <td className='table__cell note__status'>
+          {completed ? (
+            <span className='note__status--completed'>Completed</span>
+          ) : (
+            <span className='note__status--open'>Open</span>
+          )}
+        </td>
+        <td className='table__cell note__created'>{created}</td>
+        <td className='table__cell note__updated'>{updated}</td>
+        <td className='table__cell note__title'>{title}</td>
+        <td className='table__cell note__username'>{username}</td>
+
+        <td className='table__cell'>
+          <button className='icon-button table__button' onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </td>
+      </tr>
+    );
   }
-
-  const created = new Date(note.createdAt).toLocaleString('ru', {
-    day: 'numeric',
-    month: 'numeric',
-    year: '2-digit',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-
-  const updated = new Date(note.updatedAt).toLocaleString('ru', {
-    day: 'numeric',
-    month: 'numeric',
-    year: '2-digit',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-
-  const handleEdit = () => navigate(`/dash/notes/${id}`);
-
-  return (
-    <tr className='table__row'>
-      <td className='table__cell note__status'>
-        {note.completed ? (
-          <span className='note__status--completed'>Completed</span>
-        ) : (
-          <span className='note__status--open'>Open</span>
-        )}
-      </td>
-      <td className='table__cell note__created'>{created}</td>
-      <td className='table__cell note__updated'>{updated}</td>
-      <td className='table__cell note__title'>{note.title}</td>
-      <td className='table__cell note__username'>{note.username}</td>
-
-      <td className='table__cell'>
-        <button className='icon-button table__button' onClick={handleEdit}>
-          <FontAwesomeIcon icon={faPenToSquare} />
-        </button>
-      </td>
-    </tr>
-  );
-});
+);
